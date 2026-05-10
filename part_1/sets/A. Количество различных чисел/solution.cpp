5008a3a150edb2cc2e2e7e9f8440b526bfbe2bdb
Olya Gorbunova <ogorbunova@hse.ru>
2026-05-10 18:33:40 +0300

    .

---
+++ part_1/sets/A. Количество различных чисел/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+  set<int> st;
+  int c;
+  while (cin >> c)
+    st.insert(c);
+  cout << st.size() << endl;
+  return 0;
+}
